e7994214b6d30d1c2bf1420dbb00369d94a44ace
toarga <oarga.tamas@3dmap.hu>
2024-03-26 13:37:31 +0100

    Workspace init with Spring Initializr

---
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/BadiusOsicGreenTribesApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.exam.badiusosicgreentribes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadiusOsicGreenTribesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadiusOsicGreenTribesApplication.class, args);
+    }
+
+}
