85fdb73c534c514e01cc7819be8351109ce0c2f0
bodiszabolcs <szabi.bodi@gmail.com>
2024-03-27 09:52:53 +0100

    inject repositories to handlers

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/transaction/handler/ProductionHandler.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/transaction/handler/ProductionHandler.java
@@ -1,11 +1,17 @@
 package com.greenfox.exam.badiusosicgreentribes.transaction.handler;
 
 import com.greenfox.exam.badiusosicgreentribes.domain.transaction.Production;
+import com.greenfox.exam.badiusosicgreentribes.repository.transaction.ProductionRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductionHandler implements TransactionHandler<Production>{
 
+    ProductionRepository productionRepository;
+
+    public ProductionHandler(ProductionRepository productionRepository) {
+        this.productionRepository = productionRepository;
+    }
 
     @Override
     public void confirm(Production transaction) {
