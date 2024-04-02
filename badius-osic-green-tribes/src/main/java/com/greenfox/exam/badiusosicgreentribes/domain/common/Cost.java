22706d35ccadac63002cc27a7c857bf439af4126
bodiszabolcs <szabi.bodi@gmail.com>
2024-04-02 13:54:27 +0200

    add lombok annotations to classes in common package

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/common/Cost.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/common/Cost.java
@@ -2,8 +2,17 @@ package com.greenfox.exam.badiusosicgreentribes.domain.common;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cost {
 
     @Column (name = "cost_duration")
@@ -15,57 +24,5 @@ public class Cost {
     @Column (name = "cost_gold")
     private Integer gold;
 
-    public Cost() {
-    }
 
-    public Integer getDuration() {
-        return duration;
-    }
-
-    public Integer getFood() {
-        return food;
-    }
-
-    public Integer getGold() {
-        return gold;
-    }
-
-    public void setDuration(Integer duration) {
-        this.duration = duration;
-    }
-
-    public void setFood(Integer food) {
-        this.food = food;
-    }
-
-    public void setGold(Integer gold) {
-        this.gold = gold;
-    }
-
-    public static class Builder{
-        private Integer duration;
-        private Integer food;
-        private Integer gold;
-
-        public Builder duration(Integer duration){
-            this.duration = duration;
-            return this;
-        }
-        public Builder gold(Integer gold){
-            this.gold = gold;
-            return this;
-        }
-        public Builder food(Integer food){
-            this.food = food;
-            return this;
-        }
-        public Cost build(){
-            return new Cost(this);
-        }
-    }
-    private Cost(Builder builder){
-        this.duration = builder.duration;
-        this.food = builder.food;
-        this.gold = builder.gold;
-    }
 }
--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/common/User.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/common/User.java
@@ -3,12 +3,20 @@ package com.greenfox.exam.badiusosicgreentribes.domain.common;
 import com.greenfox.exam.badiusosicgreentribes.domain.kingdom.GameMap;
 import com.greenfox.exam.badiusosicgreentribes.domain.kingdom.Kingdom;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "Users")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -55,202 +63,5 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "map_id"))
     private List<GameMap> maps;
 
-    
-    public User() {
-    }
 
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Boolean getVerified() {
-        return isVerified;
-    }
-
-    public void setVerified(Boolean verified) {
-        isVerified = verified;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getLastLogin() {
-        return lastLogin;
-    }
-
-    public void setLastLogin(LocalDateTime lastLogin) {
-        this.lastLogin = lastLogin;
-    }
-
-    public List<Kingdom> getKingdoms() {
-        return kingdoms;
-    }
-
-    public void setKingdoms(List<Kingdom> kingdoms) {
-        this.kingdoms = kingdoms;
-    }
-
-    public UserRole getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
-
-    public List<GameMap> getMaps() {
-        return maps;
-    }
-
-    public void setMaps(List<GameMap> maps) {
-        this.maps = maps;
-    }
-
-    public static class Builder{
-        private Long id;
-        private String userName;
-        private String email;
-        private String firstName;
-        private String lastName;
-        private Boolean isVerified;
-        private String passwordHash;
-        private LocalDateTime createdAt;
-        private LocalDateTime lastLogin;
-        private List<Kingdom> kingdoms;
-        private UserRole userRole;
-        private List<GameMap> maps;
-
-        public Builder maps(List<GameMap> maps) {
-            this.maps = maps;
-            return this;
-        }
-
-        public Builder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder userName(String userName) {
-            this.userName = userName;
-            return this;
-        }
-
-        public Builder email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public Builder firstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public Builder lastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public Builder verified(Boolean verified) {
-            isVerified = verified;
-            return this;
-        }
-
-        public Builder passwordHash(String passwordHash) {
-            this.passwordHash = passwordHash;
-            return this;
-        }
-
-        public Builder createdAt(LocalDateTime createdAt) {
-            this.createdAt = createdAt;
-            return this;
-        }
-
-        public Builder lastLogin(LocalDateTime lastLogin) {
-            this.lastLogin = lastLogin;
-            return this;
-        }
-
-        public Builder kingdoms(List<Kingdom> kingdoms) {
-            this.kingdoms = kingdoms;
-            return this;
-        }
-
-        public Builder userRole(UserRole userRole) {
-            this.userRole = userRole;
-            return this;
-        }
-
-        public User build() {
-            return new User(this);
-        }
-    }
-
-    private User (Builder builder){
-        this.id = builder.id;
-        this.userName = builder.userName;
-        this.email = builder.email;
-        this.firstName = builder.firstName;
-        this.lastName = builder.lastName;
-        this.isVerified = builder.isVerified;
-        this.passwordHash = builder.passwordHash;
-        this.createdAt = builder.createdAt;
-        this.lastLogin = builder.lastLogin;
-        this.kingdoms = builder.kingdoms;
-        this.userRole = builder.userRole;
-        this.maps = builder.maps;
-    }
 }
