b5d3759a5c9a0fb99d40c4c82afdd751bc32c535
bodiszabolcs <szabi.bodi@gmail.com>
2024-04-02 14:01:55 +0200

    add lombok annotations to kingdom package

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/kingdom/Building.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/kingdom/Building.java
@@ -3,11 +3,16 @@ package com.greenfox.exam.badiusosicgreentribes.domain.kingdom;
 import com.greenfox.exam.badiusosicgreentribes.domain.common.Cost;
 import com.greenfox.exam.badiusosicgreentribes.domain.transaction.Transaction;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table (name = "Buildings")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Building {
 
     @Id
@@ -24,6 +29,8 @@ public class Building {
     private BuildingType type;
 
     @OneToMany
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private List<Transaction> transactions;
 
     @Embedded
@@ -32,128 +39,4 @@ public class Building {
     @ManyToOne
     private Kingdom kingdom;
 
-    public Building() {
-    }
-
-    public Kingdom getKingdom() {
-        return kingdom;
-    }
-
-    public void setKingdom(Kingdom kingdom) {
-        this.kingdom = kingdom;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-
-    public Integer getLevel() {
-        return level;
-    }
-
-    public Integer getHp() {
-        return hp;
-    }
-
-    public BuildingType getType() {
-        return type;
-    }
-
-    public List<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    public void setTransactions(List<Transaction> transactions) {
-        this.transactions = transactions;
-    }
-
-    public Cost getCost() {
-        return cost;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLevel(Integer level) {
-        this.level = level;
-    }
-
-    public void setHp(Integer hp) {
-        this.hp = hp;
-    }
-
-    public void setType(BuildingType type) {
-        this.type = type;
-    }
-
-
-    public void setCost(Cost cost) {
-        this.cost = cost;
-    }
-
-    public static class Builder{
-        private String name;
-        private Integer level;
-        private Integer hp;
-        private BuildingType type;
-        private Storage storage;
-        private List<Transaction> transactions;
-        private Cost cost;
-        private Kingdom kingdom;
-
-        public Builder(){}
-
-        public Builder kingdom(Kingdom kingdom) {
-            this.kingdom = kingdom;
-            return this;
-        }
-
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder level(Integer level) {
-            this.level = level;
-            return this;
-        }
-
-        public Builder hp(Integer hp) {
-            this.hp = hp;
-            return this;
-        }
-
-        public Builder type(BuildingType type) {
-            this.type = type;
-            return this;
-        }
-
-        public Builder transactions(List<Transaction> transactions) {
-            this.transactions = transactions;
-            return this;
-        }
-
-        public Builder cost(Cost cost) {
-            this.cost = cost;
-            return this;
-        }
-        public Building build(){
-            return new Building(this);
-        }
-    }
-    private Building(Builder builder){
-        this.name = builder.name;
-        this.level = builder.level;
-        this.hp = builder.hp;
-        this.type = builder.type;
-        this.transactions = builder.transactions;
-        this.cost = builder.cost;
-        this.kingdom = builder.kingdom;
-    }
 }
--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/kingdom/Kingdom.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/kingdom/Kingdom.java
@@ -3,9 +3,17 @@ package com.greenfox.exam.badiusosicgreentribes.domain.kingdom;
 import com.greenfox.exam.badiusosicgreentribes.domain.common.User;
 import jakarta.persistence.*;
 import jakarta.websocket.ClientEndpoint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table (name = "Kingdoms")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Kingdom {
 
     @Id
@@ -28,120 +36,4 @@ public class Kingdom {
 
     @Embedded
     private Storage storage;
-
-    public Kingdom() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public static class Builder{
-        private String name;
-        private String ownerName;
-        private Integer coordinateX;
-        private Integer coordinateY;
-        private User owner;
-        private GameMap map;
-        private Storage storage;
-
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder ownerName(String ownerName) {
-            this.ownerName = ownerName;
-            return this;
-        }
-
-        public Builder coordinateX(Integer coordinateX) {
-            this.coordinateX = coordinateX;
-            return this;
-        }
-
-        public Builder coordinateY(Integer coordinateY) {
-            this.coordinateY = coordinateY;
-            return this;
-        }
-
-        public Builder owner(User owner) {
-            this.owner = owner;
-            return this;
-        }
-
-        public Builder map(GameMap map) {
-            this.map = map;
-            return this;
-        }
-
-        public Builder storage(Storage storage) {
-            this.storage = storage;
-            return this;
-        }
-
-        public Kingdom build() {
-            return new Kingdom(this);
-        }
-    }
-
-    private Kingdom (Builder builder){
-        this.name = builder.name;
-        this.coordinateX = builder.coordinateX;
-        this.coordinateY = builder.coordinateY;
-        this.owner = builder.owner;
-        this.map = builder.map;
-        this.storage = builder.storage;
-    }
-
-
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getCoordinateX() {
-        return coordinateX;
-    }
-
-    public void setCoordinateX(Integer coordinateX) {
-        this.coordinateX = coordinateX;
-    }
-
-    public Integer getCoordinateY() {
-        return coordinateY;
-    }
-
-    public void setCoordinateY(Integer coordinateY) {
-        this.coordinateY = coordinateY;
-    }
-
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
-
-    public GameMap getMap() {
-        return map;
-    }
-
-    public void setMap(GameMap map) {
-        this.map = map;
-    }
-
-    public Storage getStorage() {
-        return storage;
-    }
-
-    public void setStorage(Storage storage) {
-        this.storage = storage;
-    }
 }
