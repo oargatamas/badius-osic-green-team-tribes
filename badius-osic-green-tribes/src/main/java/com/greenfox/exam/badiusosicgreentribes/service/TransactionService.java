c510b017a3481c0ea0defdc08e28e25ae60de487
Oravecz Gergő Gyula <oraveczgergo68+greenfoxacademy@gmail.com>
2024-04-02 11:51:39 +0200

    implemented checkTransactions functionality    fetch non complete transactions    filter expired transactions    execute effects of expired transactions

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/TransactionService.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/TransactionService.java
@@ -1,11 +1,15 @@
 package com.greenfox.exam.badiusosicgreentribes.service;
 
 import com.greenfox.exam.badiusosicgreentribes.domain.transaction.Transaction;
+import com.greenfox.exam.badiusosicgreentribes.domain.transaction.TransactionState;
 import com.greenfox.exam.badiusosicgreentribes.repository.transaction.TransactionRepository;
 import com.greenfox.exam.badiusosicgreentribes.transaction.TransactionHandlerFactory;
+import com.greenfox.exam.badiusosicgreentribes.transaction.handler.TransactionHandler;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionService {
@@ -19,11 +23,30 @@ public class TransactionService {
     }
 
     public void checkTransactions(){
-        //Todo implement method
+        List<Transaction> transactions = repository.findTransactionsByStateIs(TransactionState.SCHEDULED);
+        List<Transaction> expiredTransactions = filterExpiredTransactions(transactions);
+        expiredTransactions.forEach(transaction -> {
+            TransactionHandler<Transaction> handler = handlerFactory.getHandler(transaction);
+            if (handler != null) {
+                handler.confirm(transaction);
+                transaction.setState(TransactionState.COMPLETED);
+                repository.save(transaction);
+            } else {
+                transaction.setState(TransactionState.FAILED);
+                repository.save(transaction);
+            }
+        });
     }
 
     private List<Transaction> filterExpiredTransactions(List<Transaction> transactionList){
-        //Todo implement correct filtering based on the current system date and started/duration
-        return transactionList;
+        LocalDateTime now = LocalDateTime.now();
+        return transactionList.stream()
+                .filter(transaction -> isTransactionExpired(transaction.getStartAt(), transaction.getDuration(), now))
+                .collect(Collectors.toList());
+    }
+
+    private boolean isTransactionExpired(LocalDateTime startedAt, Integer durationInSeconds, LocalDateTime currentTime) {
+        LocalDateTime expirationTime = startedAt.plusSeconds(durationInSeconds);
+        return expirationTime.isBefore(currentTime);
     }
 }
