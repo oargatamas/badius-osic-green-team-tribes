6b9ff65ca7b2e608ea1ee740d391b9acf1e574ac
bodiszabolcs <szabi.bodi@gmail.com>
2024-04-03 15:17:56 +0200

    add lombok annotation @RequiredArgsConstructor to Spring Beans

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/transaction/handler/MovementHandler.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/transaction/handler/MovementHandler.java
@@ -2,14 +2,13 @@ package com.greenfox.exam.badiusosicgreentribes.transaction.handler;
 
 import com.greenfox.exam.badiusosicgreentribes.domain.transaction.Movement;
 import com.greenfox.exam.badiusosicgreentribes.repository.transaction.MovementRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MovementHandler implements TransactionHandler<Movement> {
     MovementRepository movementRepository;
-    public MovementHandler(MovementRepository movementRepository) {
-        this.movementRepository = movementRepository;
-    }
 
     @Override
     public void confirm(Movement transaction) {
