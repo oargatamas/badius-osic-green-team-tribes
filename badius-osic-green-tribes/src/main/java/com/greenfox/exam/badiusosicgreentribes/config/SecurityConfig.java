7d66c8817035c7d10baa3f057976a24fdb25e98f
Oravecz Gergő Gyula <oraveczgergo68+greenfoxacademy@gmail.com>
2024-04-10 11:43:42 +0200

    SecurityConfig test changes

---
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.greenfox.exam.badiusosicgreentribes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                  .authorizeHttpRequests(requests -> requests
+                  .requestMatchers("/authenticate").hasAnyRole("PLAYER", "ADMIN", "VISITOR")
+                  .anyRequest().authenticated())
+                  .formLogin(form -> form
+                          .loginPage("/login")
+                          .permitAll()
+                  )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
