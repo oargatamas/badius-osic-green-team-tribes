e95b37d97598dbd4f2bca6e5d85bad3bb47ca146
bodiszabolcs <szabi.bodi@gmail.com>
2024-04-17 16:22:33 +0200

    update damage calculation. unfinished

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/battle/DamageCalculator.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/battle/DamageCalculator.java
@@ -1,4 +1,17 @@
 package com.greenfox.exam.badiusosicgreentribes.service.battle;
 
+import com.greenfox.exam.badiusosicgreentribes.domain.battle.Troop;
+import com.greenfox.exam.badiusosicgreentribes.model.battle.Damage;
+
 public class DamageCalculator {
+    public Damage calcDamage(Troop attacker, Troop defender){
+        int attack = attacker.getQuantity() * attacker.getUnit().getStats().getAttack();
+        int defense = defender.getQuantity() * defender.getUnit().getStats().getDefense();
+        int damage = Math.max(attack - defense, 0);
+        int NoDeadUnits;
+        int NoDeadTroops;
+
+
+        return null;
+    }
 }
--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/battle/QueuedTurnSelector.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/battle/QueuedTurnSelector.java
@@ -2,15 +2,19 @@ package com.greenfox.exam.badiusosicgreentribes.service.battle;
 
 import com.greenfox.exam.badiusosicgreentribes.domain.battle.Army;
 import com.greenfox.exam.badiusosicgreentribes.domain.battle.Troop;
+import com.greenfox.exam.badiusosicgreentribes.model.battle.Damage;
 
 public class QueuedTurnSelector implements TurnSelector {
     @Override
     public Troop getAttacker(Army army) {
-        return null;
+        return army.getTroops().stream().filter(t -> t.getQuantity() > 0).findFirst().orElseThrow(() -> new IllegalStateException("No sufficient troop found"));
     }
 
     @Override
     public Troop getDefender(Army army) {
-        return null;
+        return army.getTroops().stream().filter(t -> t.getQuantity() > 0).findFirst().orElseThrow(() -> new IllegalStateException("No sufficient troop found"));
+    }
+    public void applyDamage(Damage damage, Troop attacker, Troop defender){
+
     }
 }
