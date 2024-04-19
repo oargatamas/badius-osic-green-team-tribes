afdd400afb6635f8243460d0f9e90d3ad46f402c
Oravecz Gergő Gyula <oraveczgergo68+greenfoxacademy@gmail.com>
2024-04-19 11:46:41 +0200

    Implemented calcDamage functionality in DamageCalculator

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/battle/calculator/DamageCalculator.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/battle/calculator/DamageCalculator.java
@@ -3,11 +3,21 @@ package com.greenfox.exam.badiusosicgreentribes.service.battle.calculator;
 import com.greenfox.exam.badiusosicgreentribes.domain.battle.Troop;
 import com.greenfox.exam.badiusosicgreentribes.model.battle.Damage;
 
+import java.util.Objects;
+
 public class DamageCalculator {
 
     public Damage calcDamage(Troop attacker, Troop defender){
 
+        Integer damage = attacker.getStats().getAttack() * attacker.getQuantity() - defender.getStats().getDefense() * defender.getQuantity();
+        Integer noDeadUnits = Math.min(damage / defender.getStats().getHealth(), defender.getQuantity());
+        Integer noDeadTroops = Objects.equals(noDeadUnits, defender.getQuantity()) ? 1 : 0;
 
-        return new Damage();
+        return  Damage.builder()
+                .chanceToRepost(1f)
+                .damage(damage)
+                .NoDeadUnits(noDeadUnits)
+                .NoDeadTroops(noDeadTroops)
+                .build();
     }
 }
