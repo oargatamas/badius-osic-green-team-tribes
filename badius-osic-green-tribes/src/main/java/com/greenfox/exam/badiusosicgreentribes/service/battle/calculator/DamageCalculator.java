93051d93466b2ba3cb6647f59422af2dbf8a435a
Oravecz Gergő Gyula <oraveczgergo68+greenfoxacademy@gmail.com>
2024-04-19 15:21:03 +0200

    Push for fixing

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/battle/calculator/DamageCalculator.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/service/battle/calculator/DamageCalculator.java
@@ -10,8 +10,9 @@ public class DamageCalculator {
     public Damage calcDamage(Troop attacker, Troop defender){
 
         Integer damage = attacker.getStats().getAttack() * attacker.getQuantity() - defender.getStats().getDefense() * defender.getQuantity();
-        Integer noDeadUnits = Math.min(damage / defender.getStats().getHealth(), defender.getQuantity());
+        Integer noDeadUnits = Math.min(damage / defender.getStats().getHealth() - (defender.getStats().getHealth() - defender.getCurrentHealth()), defender.getQuantity());
         Integer noDeadTroops = Objects.equals(noDeadUnits, defender.getQuantity()) ? 1 : 0;
+        damage = Objects.equals(noDeadUnits, defender.getQuantity()) ? 0 : damage % defender.getStats().getHealth();
 
         return  Damage.builder()
                 .chanceToRepost(1f)
