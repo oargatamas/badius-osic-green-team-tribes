ab6d4c5b9f8775f1d03af57cb4f1d528c57ecee8
bodiszabolcs <szabi.bodi@gmail.com>
2024-04-22 15:21:59 +0200

    create test for turnbasedbattleflow

---
+++ badius-osic-green-tribes/src/test/java/test/TurnBasedBattleFlowTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.greenfox.exam.badiusosicgreentribes.service.battle.calculator.DamageCalculator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TurnBasedBattleFlowTest {
+    @Mock
+    DamageCalculator damageCalculator;
+    @BeforeAll
+    public static void setupTests(){
+
+    }
+
+    @Test
+    void prepare() {
+    }
+
+    @Test
+    void battle() {
+    }
+}
