0848303ba59bc4150e12b3512bb607ea8b35f0a7
Dori Steiber <dora.turjan@gmail.com>
2024-04-23 10:48:28 +0200

    add isRefundable property to Movement class (for MovementHandler impl)

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/transaction/Movement.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/transaction/Movement.java
@@ -18,7 +18,7 @@ import lombok.experimental.SuperBuilder;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Movement extends Transaction{
+public class Movement extends Transaction {
 
     @ManyToOne
     private Kingdom origin;
@@ -26,4 +26,5 @@ public class Movement extends Transaction{
     private Kingdom destination;
     @OneToOne
     private Army army;
+    private Boolean isRefundable;
 }
