3e7a75b87bb631102990cfb8119eae4e8ba2d20f
toarga <oarga.tamas@3dmap.hu>
2024-04-25 11:22:43 +0200

    Configuration finished.

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/config/battle/BattleConfiguration.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/config/battle/BattleConfiguration.java
@@ -1,8 +1,13 @@
 package com.greenfox.exam.badiusosicgreentribes.config.battle;
 
+import com.greenfox.exam.badiusosicgreentribes.service.KingdomService;
+import com.greenfox.exam.badiusosicgreentribes.service.TransactionService;
 import com.greenfox.exam.badiusosicgreentribes.service.battle.BattleFlow;
 import com.greenfox.exam.badiusosicgreentribes.service.battle.TurnSelector;
 import com.greenfox.exam.badiusosicgreentribes.service.battle.calculator.DamageCalculator;
+import com.greenfox.exam.badiusosicgreentribes.service.battle.flow.QueuedTurnSelector;
+import com.greenfox.exam.badiusosicgreentribes.service.battle.flow.TurnBasedBattleFlow;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,17 +15,17 @@ import org.springframework.context.annotation.Configuration;
 public class BattleConfiguration {
 
     @Bean
-    public BattleFlow battleFlow(){
-        return null;
+    public BattleFlow battleFlow(TransactionService transactionService, BeanFactory beanFactory, KingdomService kingdomService){
+        return new TurnBasedBattleFlow(transactionService, beanFactory, kingdomService);
     }
 
     @Bean
-    public TurnSelector turnSelector(){
-        return null;
+    public TurnSelector turnSelector() {
+        return new QueuedTurnSelector();
     }
 
     @Bean
-    public DamageCalculator damageCalculator(){
-        return null;
+    public DamageCalculator damageCalculator() {
+        return new DamageCalculator();
     }
 }
