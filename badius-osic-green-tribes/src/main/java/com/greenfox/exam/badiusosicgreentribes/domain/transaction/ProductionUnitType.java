2922462aa9cf9c6ccbd03be11ebd9f72e31fa80c
Balazs Feher <radiofej@gmail.com>
2024-05-03 16:27:40 +0200

    add getCategory() to ProductionUnitType, update logic of confirm() in ProductionHandler

--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/transaction/ProductionUnitType.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/domain/transaction/ProductionUnitType.java
@@ -1,20 +1,106 @@
 package com.greenfox.exam.badiusosicgreentribes.domain.transaction;
 
 public enum ProductionUnitType {
-    BUILDING,
-    RESOURCE,
-    UNIT,
-    TOWNHALL,
-    MINE,
-    FARM,
-    BARRACK,
-    ACADEMY,
-    KNIGHT,
-    ROGUE,
-    MAGE,
-    ARCHER,
-    SQUIRE,
-    DIPLOMAT,
-    GOLD,
-    FOOD
+
+    BUILDING {
+        @Override
+        public ProductionUnitType getCategory() {
+            return BUILDING;
+        }
+    },
+    RESOURCE {
+        @Override
+        public ProductionUnitType getCategory() {
+            return RESOURCE;
+        }
+    },
+    UNIT {
+        @Override
+        public ProductionUnitType getCategory() {
+            return UNIT;
+        }
+    },
+
+    TOWNHALL {
+        @Override
+        public ProductionUnitType getCategory() {
+            return BUILDING;
+        }
+    },
+    MINE {
+        @Override
+        public ProductionUnitType getCategory() {
+            return BUILDING;
+        }
+    },
+    FARM {
+        @Override
+        public ProductionUnitType getCategory() {
+            return BUILDING;
+        }
+    },
+    BARRACK {
+        @Override
+        public ProductionUnitType getCategory() {
+            return BUILDING;
+        }
+    },
+    ACADEMY {
+        @Override
+        public ProductionUnitType getCategory() {
+            return BUILDING;
+        }
+    },
+    KNIGHT {
+        @Override
+        public ProductionUnitType getCategory() {
+            return UNIT;
+        }
+    },
+    ROGUE {
+        @Override
+        public ProductionUnitType getCategory() {
+            return UNIT;
+        }
+    },
+    MAGE {
+        @Override
+        public ProductionUnitType getCategory() {
+            return UNIT;
+        }
+    },
+    ARCHER {
+        @Override
+        public ProductionUnitType getCategory() {
+            return UNIT;
+        }
+    },
+    SQUIRE {
+        @Override
+        public ProductionUnitType getCategory() {
+            return UNIT;
+        }
+    },
+    DIPLOMAT {
+        @Override
+        public ProductionUnitType getCategory() {
+            return UNIT;
+        }
+    },
+    GOLD {
+        @Override
+        public ProductionUnitType getCategory() {
+            return GOLD;
+        }
+    },
+
+    FOOD {
+        @Override
+        public ProductionUnitType getCategory() {
+            return FOOD;
+        }
+    };
+
+
+    public abstract ProductionUnitType getCategory();
 }
--- badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/transaction/handler/ProductionHandler.java
+++ badius-osic-green-tribes/src/main/java/com/greenfox/exam/badiusosicgreentribes/transaction/handler/ProductionHandler.java
@@ -16,51 +16,21 @@ public class ProductionHandler implements TransactionHandler<Production> {
     @Override
     public void confirm(Production transaction) {
 
-        switch (transaction.getProductionType()) {
+        switch (transaction.getProductionType().getCategory()) {
+
+            case BUILDING:
+                BuildingType buildingType = BuildingType.valueOf(transaction.getProductionType().name());
+                transaction.getTargetKingdom().addBuilding(buildingType, transaction.getQuantity());
+                break;
             case GOLD:
                 transaction.getTargetKingdom().getStorage().setGold(transaction.getTargetKingdom().getStorage().getGold() + transaction.getQuantity());
                 break;
             case FOOD:
                 transaction.getTargetKingdom().getStorage().setFood(transaction.getTargetKingdom().getStorage().getFood() + transaction.getQuantity());
                 break;
-            case BUILDING: // do we actually produce plain buildings?
-                break;
-            case RESOURCE: // do we actually produce plain resource?
-                break;
-            case UNIT: // do we actually produce plain unit?
-                break;
-            case TOWNHALL:
-                transaction.getTargetKingdom().addBuilding(BuildingType.TOWNHALL, transaction.getQuantity());
-                break;
-            case MINE:
-                transaction.getTargetKingdom().addBuilding(BuildingType.MINE, transaction.getQuantity());
-                break;
-            case FARM:
-                transaction.getTargetKingdom().addBuilding(BuildingType.FARM, transaction.getQuantity());
-                break;
-            case BARRACK:
-                transaction.getTargetKingdom().addBuilding(BuildingType.BARRACK, transaction.getQuantity());
-                break;
-            case ACADEMY:
-                transaction.getTargetKingdom().addBuilding(BuildingType.ACADEMY, transaction.getQuantity());
-                break;
-            case KNIGHT:
-                transaction.getTargetKingdom().addTroop(UnitType.KNIGHT, transaction.getQuantity());
-                break;
-            case ROGUE:
-                transaction.getTargetKingdom().addTroop(UnitType.ROGUE, transaction.getQuantity());
-                break;
-            case MAGE:
-                transaction.getTargetKingdom().addTroop(UnitType.MAGE, transaction.getQuantity());
-                break;
-            case ARCHER:
-                transaction.getTargetKingdom().addTroop(UnitType.ARCHER, transaction.getQuantity());
-                break;
-            case SQUIRE:
-                transaction.getTargetKingdom().addTroop(UnitType.SQUIRE, transaction.getQuantity());
-                break;
-            case DIPLOMAT:
-                transaction.getTargetKingdom().addTroop(UnitType.DIPLOMAT, transaction.getQuantity());
+            case UNIT:
+                UnitType unitType = UnitType.valueOf(transaction.getProductionType().name());
+                transaction.getTargetKingdom().addTroop(unitType, transaction.getQuantity());
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + transaction.getProductionType());
